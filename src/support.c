70a33a47bbd34af32c58b4bb10bd82392a7836b3
John Brandwood <john.brandwood@telzey.com>
2024-03-22 21:08:20 -0400

    Apply modified Debian 05-gniibe-fix-13.patch, to fix heap-based buffer overflow in the do_msg() function
    https://sourceforge.net/p/mcpp/bugs/13/ and CVE-2019-14274
    
    Description: Fix for a bug reported to sourceforge.net #13
       by fixing error messages.
       Also, fix erroneous messages.
    Author: NIIBE Yutaka
    
    The buffer overflow was caused by do_msg() treating illegal control characters
    in the error message output as tokenized macros that needed to be expanded for
    printing.
    
    This fixes the problem by just not printing the preprocessed source code which
    contains those illegal control characters.
    
    The patch has been reworked to change the error messages to better match those
    that are output when unterminated strings are encountered. It also changes the
    other patched messages to restore the 'l' in "%02lx" because do_msg() passes a
    long integer to mcpp_fprintf().

--- src/support.c
+++ src/support.c
@@ -731,9 +731,9 @@ char *  scan_quote(
     const char * const      unterm_string
                         = "Unterminated string literal%s";
     const char * const      unterm_char
-                        = "Unterminated character constant %s%.0ld%s";
+                        = "Unterminated character constant%s";
     const char * const      empty_const
-                        = "Empty character constant %s%.0ld%s";
+                        = "Empty character constant%s";
     const char *    skip;
     size_t      len;
     int         c;
@@ -822,7 +822,7 @@ escape:
         if (diag && iscntrl( c) && ((char_type[ c] & SPA) == 0)
                 && (warn_level & 1))
             cwarn(
-            "Illegal control character %.0s0lx%02x in quotation"    /* _W1_ */
+            "Illegal control character %.0s0x%02lx in quotation"    /* _W1_ */
                     , NULL, (long) c, NULL);
         *out_p++ = c;
 chk_limit:
@@ -855,16 +855,16 @@ chk_limit:
                         goto  scan;         /* Splice the lines     */
                     /* Else end of file     */
                 } else {
-                    cerror( unterm_string, skip, 0L, NULL); /* _E_  */
+                    cerror( unterm_string, skip, 0L, NULL);         /* _E_  */
                 }
             } else if (delim == '\'') {
                 if (mcpp_mode != POST_STD && option_flags.lang_asm) {
                     /* STD, KR      */
                     if (warn_level & 1)
-                        cwarn( unterm_char, out, 0L, NULL); /* _W1_ */
+                        cwarn( unterm_char, NULL, 0L, NULL);        /* _W1_ */
                     goto  done;
                 } else {
-                    cerror( unterm_char, out, 0L, skip);    /* _E_  */
+                    cerror( unterm_char, skip, 0L, NULL);           /* _E_  */
                 }
             } else {
                 cerror( "Unterminated header name %s%.0ld%s"        /* _E_  */
@@ -875,9 +875,9 @@ chk_limit:
             if (mcpp_mode != POST_STD && option_flags.lang_asm) {
                 /* STD, KR      */
                 if (warn_level & 1)
-                    cwarn( empty_const, out, 0L, skip);     /* _W1_ */
+                    cwarn( empty_const, skip, 0L, NULL);            /* _W1_ */
             } else {
-                cerror( empty_const, out, 0L, skip);        /* _E_  */
+                cerror( empty_const, skip, 0L, NULL);               /* _E_  */
                 out_p = NULL;
                 goto  done;
             }
@@ -1774,7 +1774,7 @@ not_comment:
         default:
             if (iscntrl( c)) {
                 cerror(             /* Skip the control character   */
-    "Illegal control character %.0s0x%lx, skipped the character"    /* _E_  */
+    "Illegal control character %.0s0x%02lx, skipped the character"  /* _E_  */
                         , NULL, (long) c, NULL);
             } else {                        /* Any valid character  */
                 *tp++ = c;
